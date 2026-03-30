1ef0ba1eed6555b21cab505fe22feeb005a539a6
Igor Pytlinski <s34292@pjwstk.edu.pl>
2026-03-30 15:05:20 +0200

    commit 2

--- Exercises/LinqExercises.cs
+++ Exercises/LinqExercises.cs
@@ -16,7 +16,13 @@ public sealed class LinqExercises
     /// </summary>
     public IEnumerable<string> Task01_StudentsFromWarsaw()
     {
-        throw NotImplemented(nameof(Task01_StudentsFromWarsaw));
+        return UniversityData.Students
+            .Where(s => s.City =="Warsaw")
+            .OrderBy(s => s.LastName)
+            .ThenBy(s => s.FirstName)
+            .Select(s => $"{s.IndexNumber}: {s.FirstName} {s.LastName} ({s.City})");
+            
+    throw NotImplemented(nameof(Task01_StudentsFromWarsaw));
     }
 
     /// <summary>
@@ -30,6 +36,9 @@ public sealed class LinqExercises
     /// </summary>
     public IEnumerable<string> Task02_StudentEmailAddresses()
     {
+        return UniversityData.Students
+            .Select(s => s.Email)
+            .ToList();
         throw NotImplemented(nameof(Task02_StudentEmailAddresses));
     }
 
@@ -45,6 +54,10 @@ public sealed class LinqExercises
     /// </summary>
     public IEnumerable<string> Task03_StudentsSortedAlphabetically()
     {
+        return UniversityData.Students
+            .OrderBy(s => s.LastName)
+            .ThenBy(s => s.FirstName)
+            .Select(s => $"{s.IndexNumber}: {s.FirstName} {s.LastName}");
         throw NotImplemented(nameof(Task03_StudentsSortedAlphabetically));
     }
 
